94ce3b6d19eea11a93c899c9624ecef2cca0b088
minhee <jmh3699@naver.com>
2023-01-15 19:31:23 +0900

    [prodoo-001] 레이아웃 적용
    leftNav, topNav, calendar 컴포넌트 레이아웃 작업

--- src/App.ts
+++ src/App.ts
@@ -1,16 +1,22 @@
 import { Component } from './core/core';
-import TopNav from './components/TopNav';
 import LeftNav from './components/LeftNav';
+import TopNav from './components/TopNav';
 
 export default class App extends Component {
   render() {
-    const topNavbar = new TopNav().el;
     const leftNavbar = new LeftNav().el;
+    const topNavbar = new TopNav().el;
     const routerView = document.createElement('router-view');
+
+    const contentDiv = document.createElement('div');
+    contentDiv.classList.add('contents');
+    contentDiv.append(
+      topNavbar, 
+      routerView);
+
     this.el.append(
-      topNavbar,
       leftNavbar,
-      routerView
+      contentDiv
     );
   }
 }
