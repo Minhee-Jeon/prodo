b642fdb4d7e02972354c6f364610f8becb8e2eb1
edlee <edlee@synapsoft.co.kr>
2023-01-18 02:51:29 +0900

    [#15] fullcalendar 삽입 및 레이아웃 작업

--- src/routes/Calendar.ts
+++ src/routes/Calendar.ts
@@ -1,10 +1,45 @@
 import { Component } from '../core/core';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import Interaction from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+import '../../css/fullcalendar.css'
 
 export default class About extends Component {
   render() {
     this.el.classList.add('calendar');
-    this.el.innerHTML = /* html */ `
-    
-    `;
+    const calendarEl: HTMLDivElement = document.createElement('div');
+    calendarEl.id = "calendar";
+    this.el.append(calendarEl);
+
+    const DAY_NAMES = ['일','월','화','수','목','금','토'];
+
+    const calendar: Calendar = new Calendar(calendarEl, {
+      plugins: [Interaction, dayGridPlugin, timeGridPlugin],
+      headerToolbar: false,
+      editable: true,
+      initialView: 'timeGrid',
+      views: {
+        timeGrid: {
+          duration: {
+            weeks: 1
+          }
+        }
+      },
+      dayHeaderContent: function(arg) {
+        return {
+          html: `<div class="ci-calendar__day-name">${DAY_NAMES[arg.date.getDay()]}</div><div class="ci-calendar__day">${arg.date.getDate()}</div>`
+        }
+      },
+      weekends: true,
+      events: [
+        { title: 'Meeting', start: new Date() } // 확인용 오늘 날짜 임시 더미
+      ]
+    });
+    calendar.setOption('height', 700); // css 적용 전 임시
+    document.addEventListener("DOMContentLoaded", function() { // calendar render는 마지막에 해야 css 적용됨
+      calendar.render();
+    })
   }
 }
