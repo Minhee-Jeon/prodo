69e507413a6cec68d9ae937a971a3f41bf63ded8
giseung bang <gsbang0419@gmail.com>
2023-02-19 14:23:53 +0900

    [#7] left nav 접기 / 펼치기 구현 - 스타일 수정정

--- src/components/leftNav/UnFoldLeftNav.ts
+++ src/components/leftNav/UnFoldLeftNav.ts
@@ -3,6 +3,8 @@ import ButtonWrapper from './ButtonWrapper';
 import SearchMenu from './SearchMenu';
 import MiniCalendar from './MiniCalendar';
 import MenuList from './MenuList';
+import IconButton from '../common/IconButton';
+import Icon from '../common/Icon';
 
 export default class UnFoldLeftNav extends Component {
   render() {
@@ -10,13 +12,15 @@ export default class UnFoldLeftNav extends Component {
     const miniCalendar = new MiniCalendar().el;
     const buttonWrapper = new ButtonWrapper().el;
     const menuList = new MenuList().el;
+    const settingsButton = new IconButton({ iconEl: new Icon({ iconName: 'settings', width: '30px', height: '31px' }).el.outerHTML }).el;
 
     this.el.classList.add('left-nav-unfold');
     this.el.append(
       searchMenu,
       miniCalendar,
       buttonWrapper,
-      menuList
+      menuList,
+      settingsButton
     );
   }
 }
