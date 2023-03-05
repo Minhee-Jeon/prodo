e4080092c551e4b4448c908ce259493878b32a62
Edward Lee <38566932+ISeeTheFuture@users.noreply.github.com>
2023-03-05 15:19:34 +0900

    [#24] css 개선 (#35)
    timelabel 너비 보완
    
    전체 height값 피그마에 맞게 적용
    
    캘린더에만 스크롤 적용
    
    all day 높이 설정
    
    불필요한 스크롤 제거

--- src/routes/BigCalendar.ts
+++ src/routes/BigCalendar.ts
@@ -21,14 +21,29 @@ export default class BigCalendar extends Component {
         }
       }
     },
-    allDaySlot : false,
-    contentHeight:"auto",
+    allDaySlot : true,
     dayHeaderContent: function(timeObj) {
       return {
         html: `<div class="calendar__day-name day_no-${timeObj.date.getDay()}">${BigCalendar.DAY_NAMES[timeObj.date.getDay()]}</div><div class="calendar__date">${timeObj.date.getDate()}</div>`
       }
     },
-    slotLabelFormat: "A h", // moment 있어야 문자열로 format 수정 가능, moment 없이 common 옵션 만으로는 기능에 한계가 있음
+    slotLabelContent: function (date) {
+      let curHour = date.date.getHours();
+      if(curHour === 0) {
+        return {html: `<div class="fc-timegrid-slot-label_meridiem" ></div><div class="fc-timegrid-slot-label_hour"></div>`};
+      }
+      let meridiem = 'AM';
+      let zeroFormat = '';
+      if (curHour > 12) {
+        curHour -= 12;
+        meridiem = 'PM';
+      }
+      if (curHour < 10) {
+        zeroFormat = '0';
+
+      }
+      return {html: `<div class="fc-timegrid-slot-label_meridiem" >${meridiem}</div><div class="fc-timegrid-slot-label_hour">${zeroFormat}${curHour}</div>`};
+    },
     weekends: true,
     events: [
       { title: 'Meeting', start: new Date() } // 확인용 오늘 날짜 임시 더미
@@ -55,7 +70,7 @@ export default class BigCalendar extends Component {
   }
 
   render() {
-    this.el.classList.add('bigCalendar');
+    this.el.id = 'routes_div';
     const calendarEl: HTMLDivElement = document.createElement('div');
     calendarEl.id = "bigCalendar";
     this.el.append(calendarEl);
@@ -64,6 +79,12 @@ export default class BigCalendar extends Component {
     // calendar.setOption('height', 700); // css 적용 전 임시
     document.addEventListener("DOMContentLoaded", function() { // calendar render는 마지막에 해야 css 적용됨
       calendar.render();
+      const headerScrollers = calendarEl.getElementsByClassName('fc-scroller');
+      for(let i = 0; i < headerScrollers.length; ++i) {
+        if(!headerScrollers.item(i)?.classList.contains('fc-scroller-liquid-absolute')) {
+          headerScrollers.item(i)?.setAttribute('style','');
+        }
+      }
     })
   }
 }
